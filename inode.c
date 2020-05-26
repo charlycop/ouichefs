fbcce2a884f6dbe9fd973ed253d2185ceb4d1762
charlycop <charlycop@free.fr>
2020-05-26 21:53:44 +0200

    on dirait que ça marche dans le dir

--- inode.c
+++ inode.c
@@ -12,7 +12,6 @@
 #include <linux/buffer_head.h>
 #include <linux/slab.h>
 
-
 #include "ouichefs.h"
 #include "bitmap.h"
 
@@ -328,17 +327,15 @@ static int ouichefs_unlink(struct inode *dir, struct dentry *dentry)
 
 /*
  * Calculate the usage % of the partition
+ * dir : current directory inode
  * and return 1 if it is too much, 0 if everything is under control
  */
-ssize_t isFull(struct inode *dir)
+ssize_t isPartitionFull(struct inode *dir)
 {       
-        uint32_t space, nb_blocks_full;
-
+        uint32_t space;
         struct ouichefs_sb_info *sbi = OUICHEFS_SB(dir->i_sb);
              
-        nb_blocks_full = sbi->nr_blocks - sbi->nr_free_blocks;        
         space = (sbi->nr_free_blocks*100)/sbi->nr_blocks;
-        
         
         if(sbi->nr_blocks % sbi->nr_free_blocks)
                 --space;
@@ -351,6 +348,42 @@ ssize_t isFull(struct inode *dir)
         pr_info("Partition free space :  %u%c .... OK!\n", space,'%');
 
         return 0;
+} 
+
+/*
+ * Check if the OUICHEFS_MAX_SUBFILES is reached.
+ * dir : current directory inode
+ * and return 1 if it is reached, 0 is not.
+ */
+ssize_t isDirFull(struct inode *dir)
+{       
+        struct super_block *sb = dir->i_sb;
+	struct ouichefs_inode_info *ci_dir = OUICHEFS_INODE(dir);
+	struct buffer_head *bh = NULL;
+	struct ouichefs_dir_block *dblock = NULL;
+	uint8_t i = 0;
+
+	/* Read the directory index block on disk */
+	bh = sb_bread(sb, ci_dir->index_block);
+	if (!bh)
+		return -EIO;
+
+	dblock = (struct ouichefs_dir_block *)bh->b_data;
+
+	/* Count the number of files in this directory (block) */
+	while(i < OUICHEFS_MAX_SUBFILES && dblock->files[i].inode != 0)
+                ++i;
+	
+	brelse(bh);
+
+        if (i < OUICHEFS_MAX_SUBFILES){
+                pr_info("Subfiles quantity in this directory : %u .... OK!\n", i);
+                return 0;
+        }
+        
+        pr_warning("Too many files in this directory !! (%u) !\n", i);
+
+        return 1;
 } 
 
 unsigned long findParentOfIno(struct inode *dir, unsigned long inoParent,
@@ -394,7 +427,41 @@ unsigned long findParentOfIno(struct inode *dir, unsigned long inoParent,
         return 0;
 }
 
-unsigned long findOldest(struct inode *dir)
+unsigned long findOldestInDir(struct inode *dir)
+{
+        unsigned long ino, i = 0, min = ULONG_MAX, ino_ancien = 0;
+        struct super_block *sb = dir->i_sb;
+        struct buffer_head *bh = NULL;
+	struct ouichefs_dir_block *dir_block = NULL;
+        struct inode *inode = NULL;
+
+        /* Read parent directory index and put in RAM from the disk*/
+	bh = sb_bread(sb, OUICHEFS_INODE(dir)->index_block);
+
+        /* Get the block ifself */
+	dir_block = (struct ouichefs_dir_block *)bh->b_data;
+
+	/* Search for inode in parent index and get number of subfiles */
+        while(i < OUICHEFS_MAX_SUBFILES && dir_block->files[i].inode != 0){               
+                ino = dir_block->files[i].inode;
+                inode = ouichefs_iget(sb, ino);  
+              
+                if (S_ISDIR(inode->i_mode))
+                        continue;
+                
+                if(inode->i_mtime.tv_sec < min){
+                        min = inode->i_mtime.tv_sec;
+                        ino_ancien = ino;
+                }
+                ++i;       
+	}
+
+        pr_info("Le plus ancien du dossier est l'ino #%lu\n", ino_ancien);
+
+        return ino_ancien;
+}
+
+unsigned long findOldestInPartition(struct inode *dir)
 {
         struct inode *inode = NULL;
         struct ouichefs_sb_info *sbi = OUICHEFS_SB(dir->i_sb);
@@ -407,7 +474,7 @@ unsigned long findOldest(struct inode *dir)
                 if(ino < sbi->nr_inodes){
                         inode = ouichefs_iget(dir->i_sb, ino);
                         
-                        if (!((S_IFREG | 0644) == inode->i_mode)) // si pas fichier
+                        if (S_ISDIR(inode->i_mode)) // si pas fichier
                                 continue;
 
                         if(inode->i_mtime.tv_sec < min){
@@ -417,17 +484,21 @@ unsigned long findOldest(struct inode *dir)
                 }
         }
 
-        pr_info("Le plus ancien est l'ino #%lu\n", ino_ancien);
+        pr_info("Le plus ancien de la partition est l'ino #%lu\n", ino_ancien);
 
         return ino_ancien;
 }
 
-void shredIt(struct inode *dir, unsigned long ino){
+/*
+ * Free the dir of the inode and free the blocks of the inode
+ * dir : current directory of the file
+ * flag : 0 search for parent, 1 parent is dir
+ */
+void shredIt(struct inode *dir, unsigned long ino, uint8_t flag){
       
         struct super_block *sb = dir->i_sb;
-        struct inode *inodeToDelete = NULL, *inodeParent = NULL;
+        struct inode *inodeToDelete = NULL, *inodeParent = dir;
         struct dentry *dentry = NULL;
-
         unsigned long parent = 0;
 
         inodeToDelete = ouichefs_iget(sb, ino);     
@@ -436,25 +507,34 @@ void shredIt(struct inode *dir, unsigned long ino){
                 dentry = hlist_entry(inodeToDelete->i_dentry.first, 
                                            struct dentry, d_u.d_alias);
                 // DILEMME UTILISE-T-ON UNLINK pour montrer qu'on a bien capter la diff' ?
-                //ouichefs_unlink(dentry->d_parent->d_inode, dentry);
+                ouichefs_unlink(dentry->d_parent->d_inode, dentry);
         }
         else{  
-                parent = findParentOfIno(dir, 0, ino);
-                inodeParent = ouichefs_iget(sb, parent);
+                if(!flag){
+                        parent = findParentOfIno(dir, 0, ino);
+                        inodeParent = ouichefs_iget(sb, parent);
+                }
                 pr_info("Parent trouvé ino : %lu\n", parent);
                                 
                 scrubAndClean(inodeParent, inodeToDelete);
         }      
 }
 
-void cleanIt(struct inode *dir)
+
+/*
+ * Free the partition or the dir of the oldest file.
+ * dir : current directory
+ * flag : 0 for partition, 1 for dir
+ */
+void cleanIt(struct inode *dir, uint8_t flag)
 {
-        unsigned long ino = findOldest(dir);
+        unsigned long ino = (flag) ? 
+                             findOldestInDir(dir) : findOldestInPartition(dir);
                       
         if(!ino)
                 return;
 
-        shredIt(dir, ino);
+        shredIt(dir, ino, flag);
 
 }
 
@@ -478,9 +558,12 @@ static int ouichefs_create(struct inode *dir, struct dentry *dentry,
 	struct buffer_head *bh, *bh2;
 	int ret = 0, i;
 
-        // Pour tester
-        if(isFull(dir) || !isFull(dir))
-                cleanIt(dir);         
+        // Pour tester la partition
+        if(isPartitionFull(dir))
+                cleanIt(dir, 0);   
+        
+        if(isDirFull(dir))
+                cleanIt(dir, 1);
 
 
 	/* Check filename length */
